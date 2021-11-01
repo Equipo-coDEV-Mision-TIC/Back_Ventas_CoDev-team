4bc809398412f0b46b4e58dff634cb9a23ed3b3f
Yeison Buitrago <yeison_buitragos@hotmail.com>
2021-11-01 09:57:34 -0500

    se agregaron las rutas del crud, se instaló la libreria dotenv para utilizar buenas practicas y garantizar el no envío de la url de la base de datos al git

--- servidor.js
+++ servidor.js
@@ -2,61 +2,107 @@
 
 import Express from "express"
 import Cors from 'cors'
+import {MongoClient,ObjectId} from 'mongodb'
+import dotenv from 'dotenv'
 
+dotenv.config({path:'./.env'})
+
+const uri = process.env.DATABASE_URL; //process.env.PORT --> buena practica
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+let conexion;
 const app = Express()
 app.use(Express.json())
 app.use(Cors())
 
 
 app.get('/Productos',(req,res)=>{
-    const productos = [
-        {
-            IDproducto: "1001",Categoria: "Sofás",Descripción: "sofa reclinable cuerina",Precio: "1100000",
-        },
-        {
-            IDproducto: "1002",
-            Categoria: "Sillas Oficina",
-            Descripción: "Silla Gerente Reclinable",
-            Precio: "950000",   
-        },
-        {
-            IDproducto: "1001",Categoria: "Sofás",Descripción: "sofa reclinable cuerina",Precio: "1100000",
-        },
-        {
-            IDproducto: "1001",Categoria: "Sofás",Descripción: "sofa reclinable cuerina",Precio: "1100000",
-        },
-        {
-            IDproducto: "1001",Categoria: "Sofás",Descripción: "sofa reclinable cuerina",Precio: "1100000",
-        }   
-    ]
     console.log('Alguien hizo get en la ruta /Productos')
-    res.send(productos)
+    conexion
+    .collection('Articulo')
+    .find({}).limit(50)
+    .toArray((err,result)=>{
+        if(err){
+            res.status(500).send('Error consultando los Articulos')
+        }
+        else{
+            res.json(result);
+        }    
+    })
 })
 
 // comunicacion completa desde el front hasta el back para crear productos
 app.post('/Productos/nuevo',(req,res)=>{
     const datosProducto = req.body;
+    //console.log('llaves: ', object.keys(datosProducto))
     try{
         if(
-            Object.keys(datosProducto).includes('IDProducto') &&
+            Object.keys(datosProducto).includes('IDproducto') &&
             Object.keys(datosProducto).includes('Categoria') &&
             Object.keys(datosProducto).includes('Descripcion')&&
             Object.keys(datosProducto).includes('Precio')  
         ){
-            res.sendStatus(200);
-         }else{
-             res.sendStatus(500)
-         }  
+            conexion.collection('Articulo').insertOne(datosProducto,(err,result)=>{
+                if(err){
+                    console.error(err)
+                    res.sendStatus(500);
+                }else{
+                    console.log(result)
+                    res.sendStatus(200)
+                }  
+            })
+
+        }else{
+            res.sendStatus(500)
+        }
         }catch{
             res.sendStatus(500);
         }
-        console.log("Producto a crear: ",req.body)
-        console.log('Producto creado con exito')
         
     })
 
-// fin post
+app.patch('/Productos/editar',(req,res)=>{
+  const edicion = req.body
+  const filtroArticulo ={ _id: new ObjectId(edicion.id)}
+  delete edicion.id
+  const operacion = {
+      $set: edicion,
+  }
+  conexion.collection('Articulo').findOneAndUpdate(filtroArticulo,operacion,{upsert:true},(err,result)=>{
+    if(err){
+        console.error('Error actualizando el Articulo: ',err)
+        res.sendStatus(500)
+    }else{
+        console.log('Articulo actualizado con exito')
+        res.sendStatus(200)
+    }
+    }
+  )  
+})
 
-app.listen(5000,()=>{
-    console.log('escuchando puerto 5000')
-});
+app.delete('/Productos/Eliminar',(req,res)=>{
+    const filtroArticulo ={ _id: new ObjectId(req.body.id)}
+    conexion.collection('Articulo').deleteOne(filtroArticulo,(err,result)=>{
+        if(err){
+            console.error('Error eliminando el Articulo: ',err)
+            res.sendStatus(500)
+        }else{
+            console.log('Articulo eliminado con exito')
+            res.sendStatus(200)
+        }
+    }) 
+})
+// fin post
+const main = ()=>{
+    client.connect((err, db) => {
+        if(err) {
+            console.error('Error conectando a la base de datos')
+        }
+        conexion = db.db('TiendaTech');
+        console.log('Conexion exitosa')
+        return app.listen(process.env.PORT,()=>{ //process.env.PORT --> buena practica
+            console.log(`escuchando puerto ${process.env.PORT}`) //process.env.PORT --> buena practica
+      });
+        })
+}
+ main()
