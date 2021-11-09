42811c89847ccb2a727be60dc2cbc3baa65668b9
Yeison Buitrago <yeison_buitragos@hotmail.com>
2021-11-09 15:59:42 -0500

    agregar protección de rutas con jwt

--- servidor.js
+++ servidor.js
@@ -8,6 +8,8 @@ import {conectarBD} from "./db/db.js"
 import rutasProductos from './views/articulos/rutas.js'
 import rutasUsuarios from './views/usuarios/rutas.js'
 import rutasVentas from './views/ventas/rutas.js'
+import jwt from 'express-jwt';
+import jwks from 'jwks-rsa';
 
 
 dotenv.config({path:'./.env'})
@@ -18,6 +20,19 @@ const PORT = process.env.PORT || 5000;
 const app = Express()
 app.use(Express.json())
 app.use(Cors())
+
+var jwtCheck = jwt({
+  secret: jwks.expressJwtSecret({
+      cache: true,
+      rateLimit: true,
+      jwksRequestsPerMinute: 5,
+      jwksUri: 'https://ventas-misiontic.us.auth0.com/.well-known/jwks.json'
+}),
+audience: 'Api-Auth-TiendaTec',
+issuer: 'https://ventas-misiontic.us.auth0.com/',
+algorithms: ['RS256']
+});
+app.use(jwtCheck)
 app.use(rutasProductos)
 app.use(rutasUsuarios)
 app.use(rutasVentas)
