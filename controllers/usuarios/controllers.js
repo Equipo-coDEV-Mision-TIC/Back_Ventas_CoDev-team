3b59571cf347df78b106f77e2fd23d8e672de5e8
Yeison Buitrago <yeison_buitragos@hotmail.com>
2021-11-14 10:57:24 -0500

    configuraciones varias

--- controllers/usuarios/controllers.js
+++ controllers/usuarios/controllers.js
@@ -1,5 +1,6 @@
 import { getBD } from "../../db/db.js"
 import { ObjectId } from "mongodb";
+import jwt_decode from 'jwt-decode'
 
 const queryAllUsers = async (callback) =>{
 
@@ -18,6 +19,15 @@ const crearUsuario = async (datosUsuario, callback) =>{
         
 }
 
+const consultarOCrearUsuario = async (req, callback) => {
+    console.log('Estoy llegando a crear usuario');
+    // 6.1. obtener los datos del usuario desde el token
+    const token = req.headers.authorization.split('Bearer ')[1];
+   console.log('token',token)
+   //const token = console.log('token',req.headers.authorization)
+  console.log('token',jwt_decode(token))
+  };
+
 const editarUsuario = async (id, edicion, callback)=>{
   const filtroUsuario ={ _id: new ObjectId(id)}
   const operacion = {
@@ -35,4 +45,4 @@ const eliminarUsuario = async (id, callback)=>{
     .collection('Usuario').deleteOne(filtroUsuario,callback) 
 }
 
-export {queryAllUsers, crearUsuario, editarUsuario, eliminarUsuario}
+export {queryAllUsers, crearUsuario, editarUsuario, eliminarUsuario,consultarOCrearUsuario}
--- views/usuarios/rutas.js
+++ views/usuarios/rutas.js
@@ -1,5 +1,5 @@
 import Express from 'express'
-import { queryAllUsers, crearUsuario, editarUsuario, eliminarUsuario } from '../../controllers/usuarios/controllers.js';
+import { queryAllUsers, crearUsuario, editarUsuario, eliminarUsuario, consultarOCrearUsuario } from '../../controllers/usuarios/controllers.js';
 
 const rutasUsuarios = Express.Router();
 
@@ -22,7 +22,20 @@ rutasUsuarios.route('/Usuarios').get((req,res)=>{
 // comunicacion completa desde el front hasta el back para crear Usuario
 rutasUsuarios.route('/Usuarios').post((req,res)=>{
     crearUsuario(req.body, genercCallback(res))
+    console.log('Alguien hizo get en la ruta /Usuarios')
     })
+
+    rutasUsuarios.route('/Usuarios/self').get((req, res) => {
+      console.log('alguien hizo get en la ruta /self');
+      consultarOCrearUsuario(req, genercCallback(res));
+      console.log('usuario consultado')
+      // consultarUsuario(, genercCallback(res));
+    });
+    
+    rutasUsuarios.route('/usuarios/:id').get((req, res) => {
+      console.log('alguien hizo get en la ruta /usuarios');
+      consultarUsuario(req.params.id, genercCallback(res));
+    });
 
 rutasUsuarios.route('/Usuarios/:id').patch((req,res)=>{
   editarUsuario(req.params.id,req.body, genercCallback(res))
